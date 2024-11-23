42869eb1017e501126918fd2578b890831f6f3a6
unknown <fatmasaleh567@gmail.com>
2024-11-24 01:30:14 +0200

    change viewport of cards

--- src/app/_Components/MyProjects.tsx
+++ src/app/_Components/MyProjects.tsx
@@ -21,7 +21,7 @@ const ProjectsSection = () => {
           variants={fadeIn('up' , 0.1)}
         initial='hidden'
         whileInView='show'
-        viewport={{ once: false , amount: 0.2 }}
+        viewport={{ once: true , amount: 0.2 }}
             key={index}
           >
             <ProjectCard
