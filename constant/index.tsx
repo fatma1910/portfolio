a3f93890992cd1078aed02d0767c32f32609f26f
unknown <fatmasaleh567@gmail.com>
2024-11-29 04:04:53 +0200

    update projectts

--- constant/index.tsx
+++ constant/index.tsx
@@ -35,11 +35,11 @@ export const TAB_DATA = [
   ];
   
 
-export  const projectsData = [
+  export const projectsData = [
     {
       id: 1,
       title: "Finan Smart Dashboard",
-      description: "This dashboard project is built using Next.js, Tailwind CSS, TypeScript, Neon, Drizzle, Aceternity UI, and Chadcn. It provides a modern and visually appealing interface for displaying data and insights. The project leverages the power of these technologies to create a robust and scalable dashboard solution",
+      description: "This dashboard project is built using Next.js, Tailwind CSS, TypeScript, Neon, Drizzle, Aceternity UI, and Chadcn. It provides a modern and visually appealing interface for displaying data and insights. The project leverages the power of these technologies to create a robust and scalable dashboard solution.",
       skills: [
         "Next.js",
         "Tailwind CSS",
@@ -56,7 +56,7 @@ export  const projectsData = [
     {
       id: 2,
       title: "Bicycle Ecommerce",
-      description: "This project uses Next.js and Tailwind CSS for a responsive front-end, with Clerk handling user authentication and Sanity.io as the CMS. Built in TypeScript, it ensures type safety and scalability",
+      description: "This project uses Next.js and Tailwind CSS for a responsive front-end, with Clerk handling user authentication and Sanity.io as the CMS. Built in TypeScript, it ensures type safety and scalability.",
       skills: [
         "Next.js",
         "Tailwind CSS",
@@ -70,7 +70,7 @@ export  const projectsData = [
     {
       id: 3,
       title: "TestBranch",
-      description: "This project transforms a Figma design into a responsive web app using Next.js, TypeScript, Tailwind CSS, ShadCN UI, and Aceternity UI. It ensures pixel-perfect accuracy, strong typing for maintainability, and a modern, cohesive UI while delivering high performance and smooth functionality",
+      description: "This project transforms a Figma design into a responsive web app using Next.js, TypeScript, Tailwind CSS, ShadCN UI, and Aceternity UI. It ensures pixel-perfect accuracy, strong typing for maintainability, and a modern, cohesive UI while delivering high performance and smooth functionality.",
       skills: [
         "Next.js",
         "TypeScript",
@@ -84,6 +84,22 @@ export  const projectsData = [
     },
     {
       id: 4,
+      title: "The Book Club",
+      description: "A book review platform built using Next.js, TypeScript, Tailwind CSS, ShadCN UI, Drizzle, and Neon. This project allows users to upload books and write reviews, providing an interactive and user-friendly experience. The combination of modern technologies ensures smooth functionality, scalability, and an engaging interface.",
+      skills: [
+        "Next.js",
+        "TypeScript",
+        "Tailwind CSS",
+        "ShadCN UI",
+        "Drizzle",
+        "Neon",
+      ],
+      image: "/project/15.png",
+      gitUrl: "https://github.com/fatma1910/BookReview",
+      previewUrl: "https://book-review-59b4.vercel.app/",
+    },
+    {
+      id: 5,
       title: "Blog",
       description: "A modern web app using Next.js, TypeScript, Tailwind CSS, and Sanity.io. It leverages ShadCN UI for form elements, ensuring a responsive, dynamic, and well-designed user interface with easy content management.",
       skills: [
@@ -98,7 +114,7 @@ export  const projectsData = [
       previewUrl: "https://blog-seven-theta-47.vercel.app/",
     },
     {
-      id: 5,
+      id: 6,
       title: "Cars Showcase",
       description: "Built a responsive car showcase website using Next.js 14, with data dynamically fetched from RapidAPI. Focused on server-side rendering and performance optimization, utilizing Tailwind CSS for a sleek, mobile-friendly design.",
       skills: [
@@ -106,14 +122,13 @@ export  const projectsData = [
         "TypeScript",
         "Tailwind CSS",
         "RapidAPI",
-        
       ],
       image: "/project/5.png",
       gitUrl: "https://github.com/fatma1910/cars-showcase",
       previewUrl: "https://cars-showcase-smoky-delta.vercel.app/",
     },
     {
-      id: 6,
+      id: 7,
       title: "Todo List",
       description: "A web project built with Next.js, TypeScript, and Tailwind CSS, integrating Drizzle for Ethereum smart contract interaction, Neon for database management, and SweetAlert for custom alerts. The UI is enhanced using ShadCN UI for seamless form components, providing a responsive and interactive experience with smooth animations and easy management of blockchain data.",
       skills: [
@@ -126,13 +141,11 @@ export  const projectsData = [
         "ShadCN UI",
       ],
       image: "/project/6.png",
-  
       gitUrl: "https://github.com/fatma1910/todo-list",
       previewUrl: "https://todo-list-dun-theta.vercel.app/",
     },
-  
     {
-      id: 7,
+      id: 8,
       title: "Bouncher",
       description: "A project that converts Figma designs into a fully functional web application using Next.js, TypeScript, Tailwind CSS, and Framer Motion. It ensures a responsive, dynamic, and visually appealing user interface with smooth animations and scalable components.",
       skills: [
@@ -143,14 +156,13 @@ export  const projectsData = [
         "Figma",
       ],
       image: "/project/7.png",
-  
       gitUrl: "https://github.com/fatma1910/bouncher",
       previewUrl: "https://bouncher.vercel.app/",
     },
     {
-      id: 8,
+      id: 9,
       title: "Astra",
-      description: " Developed a modern web application using Next.js 14, featuring smooth animations with Framer Motion and secure user authentication via Google Auth. Integrated ShadCN UI and Aceternity UI to create a polished, cohesive interface. This project highlights my expertise in combining cutting-edge tools to build responsive, interactive, and userfriendly applications.",
+      description: "Developed a modern web application using Next.js 14, featuring smooth animations with Framer Motion and secure user authentication via Google Auth. Integrated ShadCN UI and Aceternity UI to create a polished, cohesive interface. This project highlights my expertise in combining cutting-edge tools to build responsive, interactive, and user-friendly applications.",
       skills: [
         "Next.js",
         "Framer Motion",
@@ -159,26 +171,24 @@ export  const projectsData = [
         "Aceternity UI",
       ],
       image: "/project/8.png",
-  
       gitUrl: "https://github.com/fatma1910/Astra",
       previewUrl: "https://astra-h5cd-acqgq0b1b-fatma1910s-projects.vercel.app/",
     },
     {
-      id: 9,
+      id: 10,
       title: "Food App",
-      description: " Created a visually appealing restaurant website using HTML, CSS, and JavaScript. Incorporated animations to enhance user interaction and create a lively, engaging experience. The project showcases a clean design and dynamic features to attract and inform customers.",
+      description: "Created a visually appealing restaurant website using HTML, CSS, and JavaScript. Incorporated animations to enhance user interaction and create a lively, engaging experience. The project showcases a clean design and dynamic features to attract and inform customers.",
       skills: [
         "HTML",
         "CSS",
         "JavaScript",
       ],
       image: "/project/9.png",
-  
       gitUrl: "https://github.com/fatma1910/Food-App",
       previewUrl: "https://food-app-eta-five.vercel.app/",
     },
     {
-      id: 10,
+      id: 11,
       title: "Weather App",
       description: "A simple weather app built with HTML, CSS, and JavaScript that allows users to check the current weather conditions by entering a city name. It fetches real-time data from a weather API and displays key details like temperature, humidity, and weather description in a clean, user-friendly interface.",
       skills: [
@@ -187,12 +197,11 @@ export  const projectsData = [
         "JavaScript",
       ],
       image: "/project/10.png",
-  
       gitUrl: "https://github.com/fatma1910/Weather-App",
       previewUrl: "https://weather-app-swart-sigma-62.vercel.app/",
     },
     {
-      id: 11,
+      id: 12,
       title: "Quiz App",
       description: "A quiz app built with HTML, CSS, and JavaScript that presents users with multiple-choice questions. It tracks scores in real-time, providing instant feedback after each question. The app offers a responsive and engaging interface, allowing users to test their knowledge across various topics.",
       skills: [
@@ -201,12 +210,11 @@ export  const projectsData = [
         "JavaScript",
       ],
       image: "/project/11.png",
-  
       gitUrl: "https://github.com/fatma1910/Quizz-App",
       previewUrl: "https://quizz-app-ochre.vercel.app/",
     },
     {
-      id: 12,
+      id: 13,
       title: "Hang-man Game",
       description: "A Hangman game built with HTML, CSS, and JavaScript where players guess a hidden word by suggesting letters. The game displays the word with blank spaces, updates the number of incorrect guesses, and provides visual feedback as the player progresses. It features a responsive design for an interactive and fun experience.",
       skills: [
@@ -215,35 +223,34 @@ export  const projectsData = [
         "JavaScript",
       ],
       image: "/project/12.png",
-  
       gitUrl: "https://github.com/fatma1910/hangman-agme-",
       previewUrl: "https://food-app-qvdv.vercel.app/",
     },
     {
-      id: 13,
+      id: 14,
       title: "Kasper",
-      description: "A simple HTML and CSS template designed to provide a clean and responsive layout for websites. It includes basic elements like a header, navigation menu, content section, and footer. The CSS is used for styling the text, colors, spacing, and overall look of the page to create a visually appealing and easy-to-navigate design.",
+      description: "A simple HTML and CSS template designed to provide a clean and responsive layout for websites. It includes basic elements like a header, navigation bar, and footer, along with placeholder content. This project demonstrates proficiency in creating structured and visually appealing web designs with pure HTML and CSS.",
       skills: [
         "HTML",
         "CSS",
       ],
       image: "/project/13.png",
-  
-      gitUrl: "https://github.com/fatma1910/kasper",
-      previewUrl: "https://kasper-sepia-one.vercel.app/",
+      gitUrl: "https://github.com/fatma1910/Kasper",
+      previewUrl: "https://kasper-eta.vercel.app/",
     },
     {
-      id: 14,
-      title: "Template 2",
-      description: "A simple HTML and CSS template designed to provide a clean and responsive layout for websites. It includes basic elements like a header, navigation menu, content section, and footer. The CSS is used for styling the text, colors, spacing, and overall look of the page to create a visually appealing and easy-to-navigate design.",
+      id: 15,
+      title: "HealthCare",
+      description: "A healthcare-related website template built with HTML, CSS, and JavaScript. This project features a responsive layout, engaging animations, and intuitive navigation. Designed to display healthcare information effectively, it demonstrates expertise in creating functional and aesthetically pleasing interfaces.",
       skills: [
         "HTML",
         "CSS",
+        "JavaScript",
       ],
       image: "/project/14.png",
-  
-      gitUrl: "https://github.com/fatma1910/second-project-",
-      previewUrl: "https://second-project-sepia.vercel.app/",
+      gitUrl: "https://github.com/fatma1910/Healthcare",
+      previewUrl: "https://healthcare-plum.vercel.app/",
     },
   ];
+  
   
