ec4856dbc8197794bce0ef3b496c4812c24fd71b
unknown <fatmasaleh567@gmail.com>
2024-12-06 08:00:11 +0200

    update

--- constant/index.tsx
+++ constant/index.tsx
@@ -69,6 +69,21 @@ export const TAB_DATA = [
     },
     {
       id: 3,
+      title: "E-Commerce App",
+      description: "A responsive E-commerce app built with Next.js, TypeScript, Redux, and Tailwind CSS, using FakeStore API for product data. It includes features like product search, filtering, and a shopping cart for a seamless shopping experience.",
+      skills: [
+        "Next.js",
+      "TypeScript",
+      "Redux",
+      "Tailwind CSS",
+      "FakeStore API",
+      ],
+      image: "/project/16.png",
+      gitUrl: "https://github.com/fatma1910/e-commerce-shop",
+      previewUrl: "https://e-commerce-shop-dusky.vercel.app/",
+    },
+    {
+      id: 4,
       title: "TestBranch",
       description: "This project transforms a Figma design into a responsive web app using Next.js, TypeScript, Tailwind CSS, ShadCN UI, and Aceternity UI. It ensures pixel-perfect accuracy, strong typing for maintainability, and a modern, cohesive UI while delivering high performance and smooth functionality.",
       skills: [
@@ -83,7 +98,7 @@ export const TAB_DATA = [
       previewUrl: "https://test-branch-one.vercel.app/",
     },
     {
-      id: 4,
+      id: 5,
       title: "The Book Club",
       description: "A book review platform built using Next.js, TypeScript, Tailwind CSS, ShadCN UI, Drizzle, and Neon. This project allows users to upload books and write reviews, providing an interactive and user-friendly experience. The combination of modern technologies ensures smooth functionality, scalability, and an engaging interface.",
       skills: [
@@ -99,7 +114,7 @@ export const TAB_DATA = [
       previewUrl: "https://book-review-59b4.vercel.app/",
     },
     {
-      id: 5,
+      id: 6,
       title: "Blog",
       description: "A modern web app using Next.js, TypeScript, Tailwind CSS, and Sanity.io. It leverages ShadCN UI for form elements, ensuring a responsive, dynamic, and well-designed user interface with easy content management.",
       skills: [
@@ -114,7 +129,7 @@ export const TAB_DATA = [
       previewUrl: "https://blog-seven-theta-47.vercel.app/",
     },
     {
-      id: 6,
+      id: 7,
       title: "Cars Showcase",
       description: "Built a responsive car showcase website using Next.js 14, with data dynamically fetched from RapidAPI. Focused on server-side rendering and performance optimization, utilizing Tailwind CSS for a sleek, mobile-friendly design.",
       skills: [
@@ -128,7 +143,7 @@ export const TAB_DATA = [
       previewUrl: "https://cars-showcase-smoky-delta.vercel.app/",
     },
     {
-      id: 7,
+      id: 8,
       title: "Todo List",
       description: "A web project built with Next.js, TypeScript, and Tailwind CSS, integrating Drizzle for Ethereum smart contract interaction, Neon for database management, and SweetAlert for custom alerts. The UI is enhanced using ShadCN UI for seamless form components, providing a responsive and interactive experience with smooth animations and easy management of blockchain data.",
       skills: [
@@ -145,7 +160,7 @@ export const TAB_DATA = [
       previewUrl: "https://todo-list-dun-theta.vercel.app/",
     },
     {
-      id: 8,
+      id: 9,
       title: "Bouncher",
       description: "A project that converts Figma designs into a fully functional web application using Next.js, TypeScript, Tailwind CSS, and Framer Motion. It ensures a responsive, dynamic, and visually appealing user interface with smooth animations and scalable components.",
       skills: [
@@ -160,7 +175,7 @@ export const TAB_DATA = [
       previewUrl: "https://bouncher.vercel.app/",
     },
     {
-      id: 9,
+      id: 10,
       title: "Astra",
       description: "Developed a modern web application using Next.js 14, featuring smooth animations with Framer Motion and secure user authentication via Google Auth. Integrated ShadCN UI and Aceternity UI to create a polished, cohesive interface. This project highlights my expertise in combining cutting-edge tools to build responsive, interactive, and user-friendly applications.",
       skills: [
@@ -175,7 +190,7 @@ export const TAB_DATA = [
       previewUrl: "https://astra-h5cd-acqgq0b1b-fatma1910s-projects.vercel.app/",
     },
     {
-      id: 10,
+      id: 11,
       title: "Food App",
       description: "Created a visually appealing restaurant website using HTML, CSS, and JavaScript. Incorporated animations to enhance user interaction and create a lively, engaging experience. The project showcases a clean design and dynamic features to attract and inform customers.",
       skills: [
@@ -188,7 +203,7 @@ export const TAB_DATA = [
       previewUrl: "https://food-app-eta-five.vercel.app/",
     },
     {
-      id: 11,
+      id: 12,
       title: "Weather App",
       description: "A simple weather app built with HTML, CSS, and JavaScript that allows users to check the current weather conditions by entering a city name. It fetches real-time data from a weather API and displays key details like temperature, humidity, and weather description in a clean, user-friendly interface.",
       skills: [
@@ -201,7 +216,7 @@ export const TAB_DATA = [
       previewUrl: "https://weather-app-swart-sigma-62.vercel.app/",
     },
     {
-      id: 12,
+      id: 13,
       title: "Quiz App",
       description: "A quiz app built with HTML, CSS, and JavaScript that presents users with multiple-choice questions. It tracks scores in real-time, providing instant feedback after each question. The app offers a responsive and engaging interface, allowing users to test their knowledge across various topics.",
       skills: [
@@ -214,20 +229,20 @@ export const TAB_DATA = [
       previewUrl: "https://quizz-app-ochre.vercel.app/",
     },
     {
-      id: 13,
+      id: 14,
       title: "Hang-man Game",
-      description: "A Hangman game built with HTML, CSS, and JavaScript where players guess a hidden word by suggesting letters. The game displays the word with blank spaces, updates the number of incorrect guesses, and provides visual feedback as the player progresses. It features a responsive design for an interactive and fun experience.",
+      description: "A Hangman game built with HTML, CSS, and JavaScript where players guess a hidden word by suggesting letters. The game displays the word with blank spaces, updates the number of incorrect guesses, and offers an interactive experience with a simple yet fun design.",
       skills: [
         "HTML",
         "CSS",
         "JavaScript",
       ],
       image: "/project/12.png",
-      gitUrl: "https://github.com/fatma1910/hangman-agme-",
-      previewUrl: "https://food-app-qvdv.vercel.app/",
+      gitUrl: "https://github.com/fatma1910/Hang-man-Game",
+      previewUrl: "https://hang-man-game-pi.vercel.app/",
     },
     {
-      id: 14,
+      id: 15,
       title: "Kasper",
       description: "A simple HTML and CSS template designed to provide a clean and responsive layout for websites. It includes basic elements like a header, navigation bar, and footer, along with placeholder content. This project demonstrates proficiency in creating structured and visually appealing web designs with pure HTML and CSS.",
       skills: [
@@ -238,19 +253,7 @@ export const TAB_DATA = [
       gitUrl: "https://github.com/fatma1910/Kasper",
       previewUrl: "https://kasper-eta.vercel.app/",
     },
-    {
-      id: 15,
-      title: "HealthCare",
-      description: "A healthcare-related website template built with HTML, CSS, and JavaScript. This project features a responsive layout, engaging animations, and intuitive navigation. Designed to display healthcare information effectively, it demonstrates expertise in creating functional and aesthetically pleasing interfaces.",
-      skills: [
-        "HTML",
-        "CSS",
-        "JavaScript",
-      ],
-      image: "/project/14.png",
-      gitUrl: "https://github.com/fatma1910/Healthcare",
-      previewUrl: "https://healthcare-plum.vercel.app/",
-    },
   ];
+  
   
   
